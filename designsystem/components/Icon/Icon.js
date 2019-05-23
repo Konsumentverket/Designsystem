68bdd2f5f9ace8f03ded429859c5eadc0b34812d
Niklas Emevi <niklas.mortensen@meridium.se>
2019-05-23 09:18:24 +0200

    replacing icons and adding icon to button

--- designsystem/components/Icon/Icon.js
+++ designsystem/components/Icon/Icon.js
@@ -1,15 +1,32 @@
-import React, { Component } from 'react';
-import Clear from './svg/clear.svg'
-import Info from './svg/info.svg'
+/** @jsx jsx */
+import { jsx } from '@emotion/core'
+import React from 'react';
+import IconCss from './Icon.css';
+
+import cars from './svg/cars.svg'
+import clothes from './svg/clothes.svg'
+import furniture from './svg/furniture.svg'
+import other from './svg/other.svg'
+import telecom from './svg/telecom.svg'
+
+
 import SVG from 'react-inlinesvg';
 
+
+
+
+
+
 const iconDefinitions = {
-    Info,
-    Clear
+    cars,
+    clothes,
+    furniture,
+    other,
+    telecom
 }
 
-const Icon = ({icon}) => (
-    <SVG src={iconDefinitions[icon]} />
+const Icon = ({icon,className}) => (
+    <SVG css={IconCss} src={iconDefinitions[icon]} className={className}></SVG>
 )
 
 export { Icon, iconDefinitions };
