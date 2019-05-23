1993898216caa91dd8d5dcf0689396ba3dbf062f
Henrik Svensson <henriksvensson@Henriks-MacBook-Pro-2.local>
2019-05-23 09:31:40 +0200

    accordion

--- designsystem/components/Accordion/Accordion.js
+++ designsystem/components/Accordion/Accordion.js
@@ -1,9 +1,50 @@
-import React from 'react';
+/** @jsx jsx */
+import { jsx } from '@emotion/core'
+import React, { useState, useEffect } from "react";
+import style from './Accordion.css';
 
-class Accordion extends React.Component {
+const Accordion = ({ content }) => {
 
-    render() {
-        return <p>Accordion</p>;
-    }
-}
-export { Accordion };
+    const [children, setchildren] = useState(content.contentCollection.items);
+    return (
+        <div css={style} className="content-accordion">
+            <h2>{content.headline}</h2>
+            <div className="accordion-content">
+                {children.map(c => (
+                    <ExpandSection
+                        content={c}
+                        key={c.title}
+                    />
+                ))}
+            </div>
+        </div>
+    );
+};
+
+
+const ExpandSection = ({ content: { headline, title, text } }) => {
+    const [open, setOpen] = useState(false);
+
+    const toggleOpen = (e) => {
+        e.preventDefault();
+        setOpen(!open);
+    };
+
+    const openClass = open ? "open" : "closed";
+
+    return (
+        <div className="content-text-section">
+            <a href="#" className={`toggle-heading ${openClass}`} onClick={(e) => toggleOpen(e)}>
+                <i className={`icon icon-expand ${openClass}`}></i>
+                <h3 className="headline">
+                    {headline}
+                </h3>
+            </a>
+            <div className={`section-content ${openClass}`}>
+                <div>{text}</div>
+            </div>
+        </div>
+    );
+};
+
+export default Accordion;
