ed33f3698b615239d4f54e0ecb61560b89a2d987
Henrik Svensson <henriksvensson@Henriks-MBP-2.lan>
2019-05-23 21:30:21 +0200

    class version

--- designsystem/components/Accordion/Accordion.js
+++ designsystem/components/Accordion/Accordion.js
@@ -4,7 +4,7 @@ import React, { useState, useEffect } from "react";
 import { ExpandSection } from "./ExpandSection";
 import style from './Accordion.css';
 
-export const Accordion = ({ content }) => {
+/* export const Accordion = ({ content }) => {
 
     const [children, setchildren] = useState(content.contentCollection.items);
     return (
@@ -20,4 +20,24 @@ export const Accordion = ({ content }) => {
             </div>
         </div>
     );
-};
+}; */
+
+export class Accordion extends React.Component {
+
+    render() {
+        return (
+            <div className="content-accordion">
+                <h2>{this.props.content.contentCollection.headline}</h2>
+                <div className="accordion-content" css={style}>
+                    {this.props.content.contentCollection.items.map(c => (
+                        <ExpandSection
+                            content={c}
+                            key={c.title}
+                        />
+                    ))}
+                </div>
+            </div>
+        );
+    }
+
+}
--- designsystem/components/Accordion/ExpandSection.js
+++ designsystem/components/Accordion/ExpandSection.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-export const ExpandSection = ({ content: { headline, title, text } }) => {
+/* export const ExpandSection = ({ content: { headline, title, text } }) => {
     const [open, setOpen] = useState(false);
 
     const toggleOpen = (e) => {
@@ -23,4 +23,35 @@ export const ExpandSection = ({ content: { headline, title, text } }) => {
             </div>
         </div>
     );
-};
+}; */
+
+export class ExpandSection extends React.Component {
+
+    componentDidMount() {
+        this.state = { open: false }
+    }
+
+    toggleOpen(e) {
+        e.preventDefault();
+        this.setState({ open: !this.state.open });
+    };
+
+    render() {
+
+        const openClass = this.state && this.state.open ? "open" : "closed";
+
+        return (
+            <div className="content-text-section">
+                <a href="#" className={`toggle-heading ${openClass}`} onClick={this.toggleOpen.bind(this)}>
+                    <i className={`icon icon-expand ${openClass}`}></i>
+                    <h3 className="headline">
+                        {this.props.content.headline}
+                    </h3>
+                </a>
+                <div className={`section-content ${openClass}`}>
+                    <div>{this.props.content.text}</div>
+                </div>
+            </div>
+        );
+    }
+}
