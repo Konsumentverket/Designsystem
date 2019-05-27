b75ef3bfd263854070f93016cccd965da75929bb
Henrik Svensson <henriksvensson@Henriks-MacBook-Pro-2.local>
2019-05-27 10:32:03 +0200

    .

--- designsystem/components/IconCard/IconCard-stories.js
+++ designsystem/components/IconCard/IconCard-stories.js
@@ -3,7 +3,7 @@ import { storiesOf } from '@storybook/react';
 import NotesLoader from '../NotesLoader';
 import { IconCard } from './IconCard';
 
-let name = 'Puff';
+let name = 'IconCard';
 storiesOf('Digitala Produkter|Komponenter', module)
     .add(name, () =>
         <IconCard icon="Furniture" text="texten" linkUrl="url/for/link" />
