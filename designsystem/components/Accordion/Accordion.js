be869ae64b3348acb3e4fd5358c2c4d972e5098b
Henrik Svensson <henriksvensson@Henriks-MacBook-Pro-2.local>
2019-05-28 13:50:01 +0200

    build

--- designsystem/components/Accordion/Accordion.js
+++ designsystem/components/Accordion/Accordion.js
@@ -7,7 +7,7 @@ import style from './Accordion.css';
 const Accordion = ({ content }) => {
 
     const [children, setchildren] = useState(content.contentCollection.items);
-    console.log(content);
+    console.log("Accordion: ", content);
 
     return (
         <div className="content-accordion">
