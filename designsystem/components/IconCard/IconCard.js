08079dca2dd9bbbac1dee1b5ca90c8fdb6b59dde
Henrik Svensson <henriksvensson@Henriks-MacBook-Pro-2.local>
2019-05-28 10:41:53 +0200

    .

--- designsystem/components/IconCard/IconCard.js
+++ designsystem/components/IconCard/IconCard.js
@@ -1,10 +1,11 @@
 import IconCardStyle from "./IconCard.css";
 import { Icon, iconDefinitions } from '../Icon/Icon';
 
-const IconCard = ({ icon, text, linkUrl }) => {
+const IconCard = ({ title, icon, text, linkUrl }) => {
     return (
         <a href={linkUrl}>
             <div className="iconCard" css={IconCardStyle}>
+                <h3>{title}</h3>
                 <Icon icon="Furniture" />
                 <span>{text}</span>
             </div>
