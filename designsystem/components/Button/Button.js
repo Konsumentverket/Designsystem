9ea0bc23e8ce93592a3f9ab440a48c64cf3298db
Henrik Svensson <henriksvensson@Henriks-MBP-2.lan>
2019-06-04 13:58:32 +0200

    button onclick

--- designsystem/components/Button/Button.js
+++ designsystem/components/Button/Button.js
@@ -8,36 +8,37 @@ class Button extends React.Component {
 
     render() {
         let classes = ["button"];
-        if(this.props.darkmode){
+        if (this.props.darkmode) {
             classes.push("darkmode")
         }
-        if(this.props.buttontype){
+        if (this.props.buttontype) {
             classes.push(this.props.buttontype)
         }
-        if(this.props.className){
+        if (this.props.className) {
             classes.push(this.props.className);
         }
-        if(this.props.disabled){
+        if (this.props.disabled) {
             classes.push("disabled");
         }
-        if(this.props.size){
+        if (this.props.size) {
             classes.push(this.props.size);
         }
-        if(this.props.icon){
+        if (this.props.icon) {
             classes.push("hasicon")
         }
 
-        return <button   
-                    css={[style,this.props.style]} 
-                    className={classes.join(" ")} 
-                    disabled={this.props.disabled} 
-                    type={this.props.type ? this.props.type : "submit"} 
-                >
-                    <span className="buttontext">
-                        {this.props.icon ? <Icon icon={this.props.icon} className="buttonicon" /> : null} 
-                        {this.props.text}
-                    </span>
-                </button>;
+        return <button
+            css={[style, this.props.style]}
+            className={classes.join(" ")}
+            disabled={this.props.disabled}
+            type={this.props.type ? this.props.type : "submit"}
+            onClick={this.props.onClick}
+        >
+            <span className="buttontext">
+                {this.props.icon ? <Icon icon={this.props.icon} className="buttonicon" /> : null}
+                {this.props.text}
+            </span>
+        </button>;
     }
 }
 export { Button };
