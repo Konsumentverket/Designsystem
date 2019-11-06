266fc4c4711230447a26271788b89272d245e1a1
Niklas Emevi <niklas.mortensen@meridium.se>
2019-11-06 13:47:38 +0100

    fixing icons

--- designsystem/icons/SystemIcons/Oval.js
+++ designsystem/icons/SystemIcons/Oval.js
@@ -6,11 +6,9 @@ export default ({ className, style, title }) => {
 
     return <svg className={className} css={style} width="9px" height="9px" viewBox="0 0 9 9">
         <title>{title}</title>
-        <g id="Page-2" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-            <g id="UX/UI-Master" transform="translate(-250.000000, -2077.000000)" fill="#B50255">
-                <circle id="Oval-Copy" cx="254.5" cy="2081.5" r="4.5"></circle>
+            <g transform="translate(-250.000000, -2077.000000)" fill="#B50255"> */}
+                <circle cx="254.5" cy="2081.5" r="4.5"></circle>
             </g>
-        </g>
     </svg>
 
 }
