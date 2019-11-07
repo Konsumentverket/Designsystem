b6457a00c246debf067187fa3648fbdce585fffe
Alexander Morell <alexander@meridium.se>
2019-11-07 08:57:12 +0100

    run hk-build-storybook

--- designsystem/sites/hallakonsument/build/main.d81983862707770ad230.bundle.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{0:function(n,o,c){c(382),c(486),n.exports=c(855)},21:function(n,o){},486:function(n,o,c){"use strict";c.r(o);c(487),c(812),c(851)}},[[0,1,2]]]);
