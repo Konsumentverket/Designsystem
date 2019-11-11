979734301c126ef9cbabf872a96df0a0e6ebe2c7
Alexandra Seppänen <alexandra@meridium.se>
2019-11-11 13:53:21 +0100

    adding tag component

--- designsystem/components/Tag/Tag.js
+++ designsystem/components/Tag/Tag.js
@@ -1,9 +1,14 @@
+/** @jsx jsx */
+import { jsx } from '@emotion/core';
 import React from 'react';
+import { tagStyle } from './Tag.css';
 
-class Tag extends React.Component {
-
-    render() {
-        return <p>Tag</p>;
-    }
+const Tag = ({ id, url, text, style }) => {
+    return <div key={id} css={[tagStyle, style]}>
+        <a href={url} >
+            <h4>{text}</h4>
+        </a>
+    </div>
 }
+
 export { Tag };
