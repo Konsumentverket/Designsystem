493868ee832eb2496197e8a7a987109820f5c7a0
Alexandra Seppänen <alexandra@meridium.se>
2019-11-12 13:37:25 +0100

    expandbutton fix

--- designsystem/components/Button/ExpandButton.js
+++ designsystem/components/Button/ExpandButton.js
@@ -2,12 +2,14 @@
 import { jsx } from '@emotion/core';
 import React from 'react';
 import { Icon } from '../Icon/Icon.js';
-import { expandButton, showLessArrow, expandArrow } from './ExpandButton.css';
+import { expandButton, showLessArrow, expandArrow, buttonContent } from './ExpandButton.css';
 
 const ExpandButton = ({onClick, text, style, expanded}) => {
     return <a css={[expandButton, style]} onClick={onClick}>
+        <div css={buttonContent}>
         <span>{text}</span>
         <Icon icon="Arrow" style={expanded ? showLessArrow : expandArrow} />
+        </div>
     </a>
 }
 
