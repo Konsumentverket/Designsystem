1484478395e14e41b38f5c79c9dba49f2d476c42
Alexandra Seppänen <alexandra@meridium.se>
2019-11-13 12:29:43 +0100

    making tags prettier for multiple rows

--- designsystem/components/Tag/Tag.css.js
+++ designsystem/components/Tag/Tag.css.js
@@ -25,3 +25,28 @@ export const tagStyle = css`
         font-weight: bold;
     }
 `;
+
+export const tagStyle2 = css`
+    border: 1px solid #48A5DF;
+    border-radius: 16px;
+    text-align: center;
+    color: ${colors.profile['blue-mid']};
+    padding: 0 5px;
+    background-color: transparent;
+    background-color: rgba(#000000, 0.6);
+    display: table;
+    min-height: 30px;
+
+    a, a:visited {
+        text-decoration: none;
+        color: ${colors.profile['blue-mid']};
+        display: table-cell;
+        vertical-align: middle;
+    }
+
+    h4 {
+        margin: 0px 10px 0 10px;
+        font-size: 16px;
+        font-weight: bold;
+    }
+`;
