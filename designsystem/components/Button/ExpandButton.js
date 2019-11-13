8cf35c86ebbdaf9a6b745a4511355c20aef67e7c
Alexandra Seppänen <alexandra@meridium.se>
2019-11-13 13:34:13 +0100

    tab navigation fix

--- designsystem/components/Button/ExpandButton.js
+++ designsystem/components/Button/ExpandButton.js
@@ -5,7 +5,7 @@ import { Icon } from '../Icon/Icon.js';
 import { expandButton, showLessArrow, buttonContent, arrow } from './ExpandButton.css';
 
 const ExpandButton = ({onClick, text, style, expanded}) => {
-    return <a css={[expandButton, style]} onClick={onClick}>
+    return <a css={[expandButton, style]} onClick={onClick} href="">
         <div css={buttonContent}>
             <span>{text}</span>
             <Icon icon="Arrow" style={[arrow, expanded && showLessArrow]} />
