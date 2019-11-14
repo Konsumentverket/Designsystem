9d3a7234fb6ea35d6c9b97fad52fea5ca8e9e2ee
Henrik Svensson <henriksvensson@Henriks-MacBook-Pro-2.local>
2019-11-14 10:19:22 +0100

    .

--- designsystem/components/IconCard/IconCard.js
+++ designsystem/components/IconCard/IconCard.js
@@ -6,7 +6,7 @@ import { Icon } from '../Icon/Icon.js';
 
 const IconCard = ({ icon, text, url, style, isH2 }) => {
     return <a href={url} css={[iconCardStyle, icon && topAreaIcon, style]} >
-        {icon && <Icon icon={icon} style={iconStyle}>Ikon</Icon>}
+        {icon && <Icon aria-hidden="true" icon={icon} style={iconStyle}>Ikon</Icon>}
         {isH2 ? <h2>{text}</h2> : <h3>{text}</h3>}
         <Icon aria-hidden="true" icon="Arrow" style={arrowStyle} />
     </a>
