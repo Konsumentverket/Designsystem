c75e7e1a63a75fbd6a163864f9c0812a8940bb52
Henrik Svensson <henriksvensson@Henriks-MacBook-Pro-2.local>
2019-11-14 10:18:40 +0100

    icon card aria-hidden

--- designsystem/components/IconCard/IconCard.js
+++ designsystem/components/IconCard/IconCard.js
@@ -4,12 +4,12 @@ import React from 'react';
 import { iconCardStyle, arrowStyle, iconStyle, topAreaIcon } from './IconCard.css';
 import { Icon } from '../Icon/Icon.js';
 
-const IconCard = ({icon, text, url, style, isH2}) => {
+const IconCard = ({ icon, text, url, style, isH2 }) => {
     return <a href={url} css={[iconCardStyle, icon && topAreaIcon, style]} >
-                    {icon && <Icon icon={icon} style={iconStyle}>Ikon</Icon>}
-                    {isH2 ? <h2>{text}</h2> : <h3>{text}</h3>}
-                    <Icon icon="Arrow" style={arrowStyle} />
-                </a>
+        {icon && <Icon icon={icon} style={iconStyle}>Ikon</Icon>}
+        {isH2 ? <h2>{text}</h2> : <h3>{text}</h3>}
+        <Icon aria-hidden="true" icon="Arrow" style={arrowStyle} />
+    </a>
 }
 
 export { IconCard };
