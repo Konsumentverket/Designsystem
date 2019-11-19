89f26a34744ec0a24d2593959f22d90b2b0fb380
Henrik Svensson <henriksvensson@Henriks-MacBook-Pro-2.local>
2019-11-19 13:55:53 +0100

    icon role and aria-label

--- designsystem/components/Button/ExpandButton.js
+++ designsystem/components/Button/ExpandButton.js
@@ -4,10 +4,10 @@ import React from 'react';
 import { Icon } from '../Icon/Icon.js';
 import { expandButton, showLessArrow, buttonContent, arrow } from './ExpandButton.css';
 
-const ExpandButton = ({onClick, text, style, expanded}) => {
+const ExpandButton = ({ onClick, text, style, expanded }) => {
     return <button css={[expandButton, style]} onClick={onClick}>
-                <span css={buttonContent}>{text} <Icon icon="Arrow" style={[arrow, expanded && showLessArrow]} /></span>
-            </button>
+        <span css={buttonContent}>{text} <Icon aria-hidden="true" icon="Arrow" style={[arrow, expanded && showLessArrow]} /></span>
+    </button>
 }
 
 export { ExpandButton };
