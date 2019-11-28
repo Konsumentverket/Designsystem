44da435b328f5dce132d31e7c019b739864898d7
Niklas Emevi <niklas.mortensen@meridium.se>
2019-11-28 13:50:02 +0100

    better story

--- designsystem/components/FocusPuff/FocusPuff-stories.js
+++ designsystem/components/FocusPuff/FocusPuff-stories.js
@@ -10,6 +10,10 @@ let name = 'FocusPuff';
 
 storiesOf('Digitala Produkter|React komponenter', module)
     .add(name, () =>
-        <FocusPuff  headline="Besök våran kampanj" text="lorem ipsum dolor lorem ipsum doloe" image="https://via.placeholder.com/304x100"/>
+        <div css={css`
+          width: 304px;
+        `}>
+          <FocusPuff  headline="Besök våran kampanj" text="lorem ipsum dolor lorem ipsum doloe" image="https://via.placeholder.com/304x100"/>
+        </div>
     , { notes: NotesLoader(name) }
     );
