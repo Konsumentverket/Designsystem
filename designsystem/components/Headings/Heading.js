37dca8e400c8edcf56919fa025167b340555b2dc
Henrik Svensson <henriksvensson@Henriks-MacBook-Pro-2.local>
2019-12-12 11:13:57 +0100

    heading otherAttr

--- designsystem/components/Headings/Heading.js
+++ designsystem/components/Headings/Heading.js
@@ -3,4 +3,4 @@ import { jsx } from '@emotion/core'
 import React from 'react';
 import HeadingStyle from './Heading.css'
 
-export const Heading = ({ text, style }) => <h1 css={[HeadingStyle, style]}>{text}</h1>
+export const Heading = ({ text, style, ...otherAttr }) => <h1 css={[HeadingStyle, style]} {...otherAttr}>{text}</h1>
