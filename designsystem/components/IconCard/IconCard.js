41652e2e9960c1632272715973a5089789f87396
Henrik Svensson <henriksvensson@Henriks-MacBook-Pro-2.local>
2020-01-10 09:37:23 +0100

    icon card ref

--- designsystem/components/IconCard/IconCard.js
+++ designsystem/components/IconCard/IconCard.js
@@ -6,12 +6,12 @@ import { EditorIcon } from '../Icon/EditorIcon.js';
 import { SubHeading } from '../Headings/SubHeading';
 
 
-const IconCard = ({ icon, text, url, style, headingLevel = 2 }) => {
-    return <a href={url} css={[iconCardStyle, style]} >
+const IconCard = React.forwardRef(({ icon, text, url, style, headingLevel = 2 }, ref) => {
+    return <a ref={ref} href={url} css={[iconCardStyle, style]} >
         {icon && <EditorIcon aria-hidden="true" icon={icon} style={iconStyle}>Ikon</EditorIcon>}
         <SubHeading level={headingLevel}>{text}</SubHeading>
         <Icon aria-hidden="true" icon="Arrow" style={arrowStyle} />
     </a>
-}
+})
 
 export { IconCard };
