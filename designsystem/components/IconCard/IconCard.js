7428c0af99e57c8568d855a5507082289589472a
Alexander Morell <alexander@meridium.se>
2020-01-14 15:42:54 +0100

    updated icon card

--- designsystem/components/IconCard/IconCard.js
+++ designsystem/components/IconCard/IconCard.js
@@ -1,16 +1,16 @@
 /** @jsx jsx */
 import React from 'react'
 import { jsx } from '@emotion/core'
-import { iconCardStyle, arrowStyle, iconStyle, topAreaIcon } from './IconCard.css';
+import { iconCardStyle, arrowStyle, iconStyle, iconText } from './IconCard.css';
 import { Icon } from '../Icon/Icon.js';
 import { EditorIcon } from '../Icon/EditorIcon.js';
 import { SubHeading } from '../Headings/SubHeading';
 
 
-const IconCard = React.forwardRef(({ icon, text, url, style, headingLevel = 2 }, ref) => {
+const IconCard = React.forwardRef(({ icon, text, url, style }, ref) => {
     return <a ref={ref} href={url} css={[iconCardStyle, style]} >
         {icon && <EditorIcon aria-hidden="true" icon={icon} style={iconStyle}>Ikon</EditorIcon>}
-        <SubHeading level={headingLevel}>{text}</SubHeading>
+        <span css={iconText}>{text}</span>
         <Icon aria-hidden="true" icon="Arrow" style={arrowStyle} />
     </a>
 })
