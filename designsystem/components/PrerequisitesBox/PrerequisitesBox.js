894a66aa2b88b7dea248e28458e262f3b1477cb2
Niklas Emevi <niklas.mortensen@meridium.se>
2020-01-24 15:58:50 +0100

    using other heading on prereq box

--- designsystem/components/PrerequisitesBox/PrerequisitesBox.js
+++ designsystem/components/PrerequisitesBox/PrerequisitesBox.js
@@ -2,13 +2,14 @@
 import { jsx } from '@emotion/core'
 import { Icon } from '../Icon/Icon'
 import { wrapper, exclamationMarkImage, heading } from './PrerequisitesBox.css'
+import { SubHeading } from '../Headings/SubHeading'
 
 export const PrerequisitesBox = ({text, children,
                             wrapperStyle}) => {
       if(children == null) return null;
       return <div css={[wrapper,wrapperStyle]}>
                 <Icon icon="Exclamationmark" style={exclamationMarkImage} />
-                {text && <strong css={heading}>{text}</strong>}
+                {text && <SubHeading text={text} styleLevel={3} />}
                 {children}
             </div>
   }
