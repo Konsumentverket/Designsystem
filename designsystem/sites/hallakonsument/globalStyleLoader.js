4c8ff08658ae792866fff2ab3e9996c9571b6ffc
Niklas Emevi <niklas.mortensen@meridium.se>
2020-02-04 16:40:19 +0100

    fixing global styles on sb and build

--- designsystem/sites/hallakonsument/globalStyleLoader.js
+++ designsystem/sites/hallakonsument/globalStyleLoader.js
@@ -1,5 +1,5 @@
 import { css,Global } from '@emotion/core'
-import {globalStyles} from '../../components/GlobalStyles/globalStyles'
+import {GlobalStyles} from '../../components/GlobalStyles/globalStyles'
 import React from 'react';
 import ReactDOM from 'react-dom';
 
@@ -7,7 +7,7 @@ var id = window.createGlobalStyleLoadingElement();
 
 ReactDOM.render(
     <div>
-        <Global styles={globalStyles()} />
+        <GlobalStyles />
         <Global styles={css`
             .sb-show-main{
                 padding: 10px;
