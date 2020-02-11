dc98bb850ef667fce6ac75a85cba7c3dae9e80d9
Alexander Morell <alexander@meridium.se>
2020-02-11 13:21:22 +0100

    updated button style

--- designsystem/components/Button/Button.css.js
+++ designsystem/components/Button/Button.css.js
@@ -1,5 +1,6 @@
 import { css } from '@emotion/core'
 import { colors } from '../Colors/Colors';
+import { medium } from '../GlobalStyles/globalStyles';
 
 const disabled = css` 
         color:${colors.common.grayDark};
@@ -16,8 +17,15 @@ export const buttonStyle = css`
       padding:1.6rem 2.4rem;
       color: ${colors.common.white};
       border-radius:.8rem;
+      width:100%;
       cursor:pointer;
       background-color:${colors.theme1.mid};
+
+      ${medium}{
+          width:auto;
+          text-align:left;
+      }
+
       &:hover {
         background-color:${colors.theme1.midDark};
         text-decoration:underline;
@@ -80,29 +88,42 @@ export const invertedSecondaryBackgroundStyle = css`
 `;
 
 export const buttonIconLeft = css`
-        position:relative;
+    position:relative;
+    svg {
+         padding-right:.8rem;
+     }
+     ${medium}{
         padding-left:4.8rem;
-        svg{
+        svg {
             left:1.6rem;
         }
+      } 
 `
 
 export const buttonIconRight = css`
-        position:relative;
+    position:relative;
+    svg {
+        padding-left:.8rem;
+    } 
+    ${medium}{
         padding-right:4.8rem;
         svg {
             right:1.6rem;
         }
+    } 
 `
 
 
 export const iconStyle = css`
-    position:absolute; 
+    
     fill:${colors.common.white};
     width: 2.4rem;
     height: 2.4rem;
-    top: calc(50% - 1.2rem);
+    top: 1.8rem;
     vertical-align: top;
+    ${medium}{
+        position:absolute; 
+      }
 `
 
 
