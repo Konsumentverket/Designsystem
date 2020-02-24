cc8640f386e2ae490465489e82f8d211f8a20d77
Fredrik Granvik <fredrik.granvik@tieto.com>
2020-02-24 14:03:39 +0100

    minor css fix

--- designsystem/components/ValidationResponse/VaildationResponse.css.js
+++ designsystem/components/ValidationResponse/VaildationResponse.css.js
@@ -5,10 +5,12 @@ export const wrapper = success => css`
   display: flex;
   color: ${success ? colors.common.success : colors.common.warning};
   font-size: 1.4rem;
-  line-height: 2.4rem;
   svg {
     align-content: center;
     margin-right: 0.4rem;
     fill: ${success ? colors.common.success : colors.common.warning};
+  }
+  p {
+    line-height: 2.4rem;
   }
 `
