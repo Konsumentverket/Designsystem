41b541e0a4e105eb47eea8e860798e4eda34e60d
Alexandra Seppänen <alexandra@meridium.se>
2020-02-25 10:33:23 +0100

    adding option to style a tag as button, color fix

--- designsystem/components/Colors/Colors.js
+++ designsystem/components/Colors/Colors.js
@@ -36,7 +36,7 @@ const defaultTheme = {
         "white": "#ffffff",
         "black": "#000000",
         "warning": "#E7342B",
-        "success": "007A3E",
+        "success": "#007A3E",
         "cyan": "#92DCFA"
     }
 }
