9c3d6b7385fc117a39621ac02905a37dcc6fb04a
Niklas Emevi <niklas.mortensen@meridium.se>
2020-03-26 13:28:54 +0100

    build

---
+++ designsystem/sites/hallakonsument/build/main.28a9ebf3675a9ae43ccc.bundle.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{0:function(n,o,c){c(394),c(844),n.exports=c(471)},3:function(n,o){},471:function(n,o,c){"use strict";c.r(o);c(472),c(840),c(922)}},[[0,1,2]]]);
