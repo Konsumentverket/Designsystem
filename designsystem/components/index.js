c4d36d1405a473ab54295db254b68b1d66bf743f
Niklas Emevi <niklas.mortensen@meridium.se>
2020-05-06 09:23:22 +0200

    index cleanup and build

--- designsystem/components/index.js
+++ designsystem/components/index.js
@@ -1,31 +1,45 @@
-export { Button } from './Button/Button.js'
-export { FormSearchField } from './FormFields/FormSearchField.js'
-export { TextArea } from './TextArea/TextArea.js'
+//styly stuff
+export { GlobalStyles, LinkWrapperColorStyle, LinkWrapperInvertedColorStyle, ElementLinkColorStyle, ElementLinkInvertedColorStyle } from './GlobalStyles/GlobalStyles'
 export { colors } from './Colors/Colors.js'
 export { spacing } from './Spacing/Spacing.js'
+
+//dates and time
+export { DateFormat, Formats } from './DateFormat/DateFormat.js'
+
+// icon stuff
 export { Icon } from './Icon/Icon.js'
 export { checkPath } from '../icons/SystemIcons/Check'
 export { EditorIcon } from './Icon/EditorIcon.js' 
+
+//headings
+export { Heading } from './Headings/Heading.js'
+export { SubHeading } from './Headings/SubHeading.js'
+
+//cards, puffs, boxes
 export { IconCard } from './IconCard/IconCard.js'
 export { Tag } from './Tag/Tag.js'
 export { TagBlock } from './Tag/TagBlock.js'
-export { ExpandButton } from './Button/ExpandButton.js'
-export { InputRadio } from './InputRadio/InputRadio.js'
 export { PrerequisitesBox } from './PrerequisitesBox/PrerequisitesBox'
-export { GlobalStyles, LinkWrapperColorStyle, LinkWrapperInvertedColorStyle, ElementLinkColorStyle, ElementLinkInvertedColorStyle } from './GlobalStyles/GlobalStyles'
-export { Heading } from './Headings/Heading.js'
-export { SubHeading } from './Headings/SubHeading.js'
-export { InputText } from './InputText/InputText.js'
 export { FocusPuff } from './FocusPuff/FocusPuff.js'
 export { CampaignFocusPuff } from './FocusPuff/CampaignFocusPuff.js'
+export { FactBox } from './FactBox/FactBox.js'
+export { LinkCard } from './LinkCard/LinkCard.js'
+export { LinkTextCard } from './LinkCard/LinkTextCard.js'
+
+//form elements
+export { InputRadio } from './InputRadio/InputRadio.js'
+export { InputText } from './InputText/InputText.js'
+export { Button } from './Button/Button.js'
+export { FormSearchField } from './FormFields/FormSearchField.js'
+export { TextArea } from './TextArea/TextArea.js'
 export { FormCheckbox } from './FormFields/FormCheckbox.js'
 export { FormRadiobutton } from './FormFields/FormRadiobutton.js'
 export { FormWrapper } from './FormFields/FormWrapper'
 export { ValidationResponse } from './ValidationResponse/ValidationResponse.js'
-export { Pagination } from './Pagination/Pagination.js'
-export { FactBox } from './FactBox/FactBox.js'
-export { Source } from './Source/Source.js'
 export { Dropdown } from './Dropdown/Dropdown.js'
-export { DateFormat, Formats } from './DateFormat/DateFormat.js'
-export { LinkCard } from './LinkCard/LinkCard.js'
-export { LinkTextCard } from './LinkCard/LinkTextCard.js'
+export { ExpandButton } from './Button/ExpandButton.js'
+
+//misc
+export { Source } from './Source/Source.js'
+export { Pagination } from './Pagination/Pagination.js'
+export { WithContentExpander } from './WithContentExpander/WithContentExpander'
