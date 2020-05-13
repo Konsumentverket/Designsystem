0b5724cf975c8a3f5af4c068e6d4d95f4e2b1b07
Niklas Emevi <niklas.mortensen@meridium.se>
2020-05-13 15:33:51 +0200

    removing element val check and adding some boldness

--- dist/main.es.js
+++ dist/main.es.js
@@ -6231,7 +6231,7 @@ function _templateObject2$a() {
 }
 
 function _templateObject$a() {
-  var data = _taggedTemplateLiteral(["\n\n    width: 100%;\n    position: relative;\n    border-radius: 0.8rem;\n    border: 2px solid transparent;\n    display:flex;\n    align-items: center;\n    background-color: ", ";\n    &.inputHasValue{\n        border: 2px solid ", ";\n        text-decoration: underline;\n    }\n\n    &:hover{\n        border: 2px solid ", ";\n        .radiolabel {\n            text-decoration: underline;\n        }\n    }\n    \n    &:focus-within{\n        .tabnav &{\n            outline-offset: 2px;\n            outline: 3px solid ", "\n        }\n    }\n    \n\n"]);
+  var data = _taggedTemplateLiteral(["\n\n    width: 100%;\n    position: relative;\n    border-radius: 0.8rem;\n    border: 2px solid transparent;\n    display:flex;\n    align-items: center;\n    background-color: ", ";\n    &.inputHasValue{\n        border: 2px solid ", ";\n        text-decoration: underline;\n        font-weight: bold;\n    }\n\n    &:hover{\n        border: 2px solid ", ";\n        .radiolabel {\n            text-decoration: underline;\n        }\n    }\n    \n    &:focus-within{\n        .tabnav &{\n            outline-offset: 2px;\n            outline: 3px solid ", ";\n        }\n    }\n    \n\n"]);
 
   _templateObject$a = function _templateObject() {
     return data;
@@ -6257,7 +6257,7 @@ var InputRadio = function InputRadio(_ref) {
   var inputRef = useRef(null);
   return jsx("div", {
     css: [fieldWrapper, fieldWrapperStyle],
-    className: inputRef != null && inputRef.current != null && inputRef.current.checked || checked ? "inputHasValue" : ""
+    className: checked ? "inputHasValue" : ""
   }, jsx("label", {
     className: "radiolabel",
     css: [fieldLabel, fieldLabelStyle],
--- dist/main.js
+++ dist/main.js
@@ -6238,7 +6238,7 @@ function _templateObject2$a() {
 }
 
 function _templateObject$a() {
-  var data = _taggedTemplateLiteral(["\n\n    width: 100%;\n    position: relative;\n    border-radius: 0.8rem;\n    border: 2px solid transparent;\n    display:flex;\n    align-items: center;\n    background-color: ", ";\n    &.inputHasValue{\n        border: 2px solid ", ";\n        text-decoration: underline;\n    }\n\n    &:hover{\n        border: 2px solid ", ";\n        .radiolabel {\n            text-decoration: underline;\n        }\n    }\n    \n    &:focus-within{\n        .tabnav &{\n            outline-offset: 2px;\n            outline: 3px solid ", "\n        }\n    }\n    \n\n"]);
+  var data = _taggedTemplateLiteral(["\n\n    width: 100%;\n    position: relative;\n    border-radius: 0.8rem;\n    border: 2px solid transparent;\n    display:flex;\n    align-items: center;\n    background-color: ", ";\n    &.inputHasValue{\n        border: 2px solid ", ";\n        text-decoration: underline;\n        font-weight: bold;\n    }\n\n    &:hover{\n        border: 2px solid ", ";\n        .radiolabel {\n            text-decoration: underline;\n        }\n    }\n    \n    &:focus-within{\n        .tabnav &{\n            outline-offset: 2px;\n            outline: 3px solid ", ";\n        }\n    }\n    \n\n"]);
 
   _templateObject$a = function _templateObject() {
     return data;
@@ -6264,7 +6264,7 @@ var InputRadio = function InputRadio(_ref) {
   var inputRef = React.useRef(null);
   return core.jsx("div", {
     css: [fieldWrapper, fieldWrapperStyle],
-    className: inputRef != null && inputRef.current != null && inputRef.current.checked || checked ? "inputHasValue" : ""
+    className: checked ? "inputHasValue" : ""
   }, core.jsx("label", {
     className: "radiolabel",
     css: [fieldLabel, fieldLabelStyle],
