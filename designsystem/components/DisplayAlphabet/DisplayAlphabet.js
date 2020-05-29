20131007e19528d10618b89daecd1e62f827ca3c
Emil Larsson <emil.larsson@tieto.com>
2020-05-29 14:54:02 +0200

    Adding new component

---
+++ designsystem/components/DisplayAlphabet/DisplayAlphabet.js
@@ -0,0 +1,41 @@
+
+/** @jsx jsx */
+import { jsx } from '@emotion/core'
+import React from 'react'
+import * as css from './DisplayAlphabet.css.js'
+import { vi } from 'date-fns/locale'
+
+const alphabet = [
+  'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+  'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+  'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
+  'Y', 'Z', 'Å', 'Ä', 'Ö'
+]
+
+export const DisplayAlphabet = ({
+  onClickLetter,
+  onClickShowAll,
+  activeLetter,
+  visibleLetters
+}) => <div>
+    <div css={css.alphabetWrapper}>
+      {alphabet.map((letter, i) =>
+        <a
+          tabIndex={(visibleLetters && Array.isArray(visibleLetters)) ? !visibleLetters.some(v => v === letter) ? '-1' : null : null}
+          css={[
+            css.link,
+            (visibleLetters && Array.isArray(visibleLetters)) ? !visibleLetters.some(v => v === letter) ? css.invalidLetter : null : null,
+            letter === activeLetter ? css.activeLetter : null
+          ]}
+          href={visibleLetters.some(v => v === letter) ? `?letter=${letter}` : `/`}
+          data-letter={letter}
+          onClick={onClickLetter}
+        >{letter}</a>)}
+    </div>
+    <div css={css.linkShowAllWrapper}>
+      <a css={css.linkShowAll}
+        href={`?letter=alla`}
+        onClick={onClickShowAll}
+      >Visa Alla A-Ö</a>
+    </div>
+  </div>
