2a4ff0a02617c0925bd5d004d9fc00ab77038a03
Niklas Emevi <kneeclass@hotmail.com>
2020-06-03 14:00:21 +0200

    some withcontentexpander focus shifts and some overflow issues

--- designsystem/components/WithContentExpander/WithContentExpander.css.js
+++ designsystem/components/WithContentExpander/WithContentExpander.css.js
@@ -140,9 +140,11 @@ export const ComponentWrapperStyle = css`
 
   .expand-section {
     max-height: 0;
-    overflow: hidden;
+    overflow: visible;
     height: auto;
     transition: all 0.3s ease-in-out;
+    display: flex;
+    flex-direction: column;
 
     &.expanded {
       max-height: 100%;
@@ -182,8 +184,8 @@ export const collapseButtonStyle = css`
   border-radius: 3.2rem;
   transition: visibility 0.1s, opacity 0.1s ease-in-out;
   font-weight: 700;
-  float: right;
   margin-bottom: 1.6rem;
+  margin-left:auto;
 
   cursor: pointer;
 
