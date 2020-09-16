5631c61ba362eb3bb198bd06add57371e5eac2f2
Emil Larsson <emil.larsson@tieto.com>
2020-09-16 08:20:08 +0200

    Build

--- dist/main.es.js
+++ dist/main.es.js
@@ -894,7 +894,7 @@ var List = (function (_ref) {
     className: className,
     css: style,
     height: "32px",
-    viewBox: "0 0 32 32",
+    viewBox: "0 0 24 24",
     width: "32px"
   }, otherAttr), jsx("title", null, title), jsx("path", {
     d: "M9.62498437,14.5 L22.875,14.5 C23.4963281,14.5 24,13.9963281 24,13.375 L24,9.625 C24,9.00367188 23.4963281,8.5 22.875,8.5 L9.62498437,8.5 C9.00365625,8.5 8.49998437,9.00367188 8.49998437,9.625 L8.49998437,13.375 C8.49998437,13.9963281 9.00365625,14.5 9.62498437,14.5 M8.49998437,2.125 L8.49998437,5.875 C8.49998437,6.49632812 9.00365625,7 9.62498437,7 L22.875,7 C23.4963281,7 24,6.49632812 24,5.875 L24,2.125 C24,1.50367188 23.4963281,1 22.875,1 L9.62498437,1 C9.00365625,1 8.49998437,1.50367188 8.49998437,2.125 M9.62498437,22 L22.875,22 C23.4963281,22 24,21.4963281 24,20.875 L24,17.125 C24,16.5036719 23.4963281,16 22.875,16 L9.62498437,16 C9.00365625,16 8.49998437,16.5036719 8.49998437,17.125 L8.49998437,20.875 C8.49998437,21.4963281 9.00365625,22 9.62498437,22 M5.87498438,1 L1.125,1 C0.503671875,1 0,1.50367188 0,2.125 L0,5.875 C0,6.49632812 0.503671875,7 1.125,7 L5.87498438,7 C6.4963125,7 6.99998438,6.49632812 6.99998438,5.875 L6.99998438,2.125 C6.99998438,1.50367188 6.4963125,1 5.87498438,1 M0,17.125 L0,20.875 C0,21.4963281 0.503671875,22 1.125,22 L5.87498438,22 C6.4963125,22 6.99998438,21.4963281 6.99998438,20.875 L6.99998438,17.125 C6.99998438,16.5036719 6.4963125,16 5.87498438,16 L1.125,16 C0.503671875,16 0,16.5036719 0,17.125 M6.99998438,9.625 L6.99998438,13.375 C6.99998438,13.9963281 6.4963125,14.5 5.87498438,14.5 L1.125,14.5 C0.503671875,14.5 0,13.9963281 0,13.375 L0,9.625 C0,9.00367188 0.503671875,8.5 1.125,8.5 L5.87498438,8.5 C6.4963125,8.5 6.99998438,9.00367188 6.99998438,9.625"
@@ -3485,7 +3485,7 @@ var InputText = React.forwardRef(function (_ref, _ref2) {
 });
 
 function _templateObject8$7() {
-  var data = _taggedTemplateLiteral(["\n    fill:", ";\n    width: 1.6rem;\n    height: 1.6rem;\n    vertical-align: bottom;\n    ", " {\n        height: 2.4rem;\n        width: 2.4rem;\n        vertical-align: bottom;\n    }\n"]);
+  var data = _taggedTemplateLiteral(["\n    position: relative;\n    fill:", ";\n    width: 1.6rem;\n    height: 1.6rem;\n    vertical-align: baseline;\n    top: 2px;\n    ", " {\n        top: 0;\n    }\n"]);
 
   _templateObject8$7 = function _templateObject8() {
     return data;
@@ -3495,7 +3495,7 @@ function _templateObject8$7() {
 }
 
 function _templateObject7$7() {
-  var data = _taggedTemplateLiteral(["\n    position:relative;\n    svg {\n        padding-left:.8rem;\n    }\n"]);
+  var data = _taggedTemplateLiteral(["\n    svg {\n        padding-left:.8rem;\n    }\n"]);
 
   _templateObject7$7 = function _templateObject7() {
     return data;
@@ -3505,7 +3505,7 @@ function _templateObject7$7() {
 }
 
 function _templateObject6$9() {
-  var data = _taggedTemplateLiteral(["\n    position:relative;\n    svg {\n        padding-right:.8rem;\n     }\n"]);
+  var data = _taggedTemplateLiteral(["\n    svg {\n        padding-right:.8rem;\n     }\n"]);
 
   _templateObject6$9 = function _templateObject6() {
     return data;
@@ -3545,7 +3545,7 @@ function _templateObject3$b() {
 }
 
 function _templateObject2$c() {
-  var data = _taggedTemplateLiteral(["\n    text-decoration: none;\n    text-align: center;\n    display: inline-block;\n    vertical-align: middle;\n    font-size:1.6rem;\n    font-weight:500;\n    padding: .8rem;\n    color: ", ";\n    border-radius:.8rem;\n    width:100%;\n    box-sizing: border-box;\n    cursor:pointer;\n    background-color:", ";\n\n    svg {\n        fill: ", ";\n        flex-shrink: 0;\n    }\n\n    ", "{\n        font-size:2.1rem;\n        line-height:2.4rem;\n        width:fit-content;\n        padding:1.6rem 2.4rem;\n    }\n\n    &:hover, &.selectedButtonStyle {\n        background-color:", ";\n        text-decoration:underline;\n    }\n    &:active{\n        background-color:", ";\n        text-decoration:underline;\n    }\n    &:disabled{\n        ", "\n    }\n"]);
+  var data = _taggedTemplateLiteral(["\n    text-decoration: none;\n    text-align: center;\n    display: inline-block;\n    font-size:1.6rem;\n    line-height:2.4rem;\n    font-weight:500;\n    padding: .8rem 2.4rem;\n    color: ", ";\n    border-radius:.8rem;\n    width:100%;\n    box-sizing: border-box;\n    cursor:pointer;\n    background-color:", ";\n\n    svg {\n        fill: ", ";\n        flex-shrink: 0;\n    }\n\n    ", "{\n        font-size:2.1rem;\n        line-height:2.4rem;\n        width:fit-content;\n        padding:1.6rem 2.4rem;\n    }\n\n    &:hover, &.selectedButtonStyle {\n        background-color:", ";\n        text-decoration:underline;\n    }\n    &:active{\n        background-color:", ";\n        text-decoration:underline;\n    }\n    &:disabled{\n        ", "\n    }\n"]);
 
   _templateObject2$c = function _templateObject2() {
     return data;
@@ -3662,7 +3662,7 @@ function _templateObject6$a() {
 }
 
 function _templateObject5$a() {
-  var data = _taggedTemplateLiteral(["\n    white-space: nowrap;\n    width:auto;\n    display:flex;\n    align-items: center;\n    border-radius: 0 .8rem .8rem 0;\n    margin-right:-.8rem;\n"]);
+  var data = _taggedTemplateLiteral(["\n    white-space: nowrap;\n    width:auto;\n    align-items: center;\n    border-radius: 0 .8rem .8rem 0;\n    margin-right:-.8rem;\n    padding: .8rem;\n"]);
 
   _templateObject5$a = function _templateObject5() {
     return data;
