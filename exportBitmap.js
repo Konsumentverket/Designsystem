11d121631309c630efae53c48615a806d982fe37
Emil Larsson <emil.larsson@tieto.com>
2020-12-02 14:51:04 +0100

    Add file for webpack magic

---
+++ exportBitmap.js
@@ -0,0 +1,47 @@
+const fs = require('fs')
+
+const readline = require('readline');
+const file = './.bitmap'
+
+const getFile = async pathToFile => {
+  return new Promise((resolve, reject) => {
+    let counter = 0
+    let file = ''
+    const readable = fs.createReadStream(pathToFile);
+    const reader = readline.createInterface({ input: readable });
+    reader.on('line', line => {
+      counter++
+      if (counter > 2) {
+        file += line
+      }
+    })
+    reader.on('close', () => {
+      resolve(file)
+    })
+  })
+}
+
+const start = `
+  var path = require('path');
+  const devAliases = isProd => isProd ? null : {
+`
+
+const end = `
+}
+
+module.exports = devAliases
+`
+
+const parseFile = async data => {
+  const res = Object
+    .keys(data)
+    .map(item => item.startsWith('konsumentverket') ? `   '${item}': path.resolve('../../../', '${data[item].mainFile}'),` : null)
+    .join('\n')
+  return start + res + end
+}
+
+
+getFile(file)
+  .then(res => JSON.parse(res))
+  .then(res => parseFile(res))
+  .then(res => console.log(res))
