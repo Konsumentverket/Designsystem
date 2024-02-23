54859a5ddc3493bf070f5a4deff9640e8b40c4aa
Ludwig Lillieborg <ludwig.lillieborg@frojd.se>
2024-02-23 15:16:36 +0100

    Fix components path

--- designsystem/bit-cloud/components/focus-puff/focus-puff.js
+++ designsystem/bit-cloud/components/focus-puff/focus-puff.js
@@ -2,7 +2,7 @@
 import { jsx } from '@emotion/core';
 import React from 'react';
 import * as css from './focus-puff.css.js';
-import { External } from '@designsystem-scope/utils/';
+import { External } from '@designsystem-scope/utils';
 
 const FocusPuff = React.forwardRef(({
   headline,
