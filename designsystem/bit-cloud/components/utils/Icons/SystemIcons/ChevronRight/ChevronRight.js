e21d78f3cda83b3e508c040698f19c9de96585e0
jakobg123 <jakob.gauffin45@gmail.com>
2024-06-03 16:05:50 +0200

    Fix ChevronRight to the icon thats in the design

--- designsystem/bit-cloud/components/utils/Icons/SystemIcons/ChevronRight/ChevronRight.js
+++ designsystem/bit-cloud/components/utils/Icons/SystemIcons/ChevronRight/ChevronRight.js
@@ -1,9 +1,14 @@
 /** @jsx jsx */
 import { jsx } from '@emotion/core'
-import React from 'react';
 
 export const ChevronRight = ({ className, style, title, ...otherAttr }) => {
-    return <svg className={className} css={style} {...otherAttr} xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18" fill="none">
-        <path d="M5.27253 8.91581L10.5391 4.24227C10.9031 3.91924 11.4917 3.91924 11.8518 4.24227L12.727 5.0189C13.091 5.34192 13.091 5.86426 12.727 6.18385L8.99395 9.49656L12.727 12.8093C13.091 13.1323 13.091 13.6546 12.727 13.9742L11.8557 14.7577C11.4917 15.0808 10.9031 15.0808 10.5429 14.7577L5.2764 10.0842C4.90851 9.76117 4.90851 9.23883 5.27253 8.91581Z" fill="#0061C2" />
-    </svg>
+    return (
+      <svg className={className} width="18" height="18" viewBox="0 0 18 18" fill="none" style={style} {...otherAttr}
+           xmlns="http://www.w3.org/2000/svg">
+        <title>{title}</title>
+          <path id="Vector"
+                d="M12.7275 9.08419L7.46094 13.7577C7.09693 14.0808 6.50832 14.0808 6.14818 13.7577L5.27301 12.9811C4.909 12.6581 4.909 12.1357 5.27301 11.8162L9.00605 8.50344L5.27301 5.19072C4.909 4.8677 4.909 4.34536 5.27301 4.02577L6.14431 3.24227C6.50832 2.91924 7.09693 2.91924 7.45707 3.24227L12.7236 7.91581C13.0915 8.23883 13.0915 8.76117 12.7275 9.08419Z"
+                />
+      </svg>
+    )
 }
