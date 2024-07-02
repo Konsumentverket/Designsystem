74bbd0bc706bb6ae4b5301b83e5cb85b431df0ef
jakobg123 <jakob.gauffin45@gmail.com>
2024-07-02 21:47:46 +0200

    bit delete header since its not used as a bit component

--- designsystem/bit-cloud/components/header/header.css.js
+++
@@ -1,6 +0,0 @@
-import { css } from '@emotion/core'
-import { newColors } from '@konsumentverket-sverige/designsystem.utils';
-
-export const headerStyle = css`
-    background: ${newColors.primaries.kovBlue};
-`;
--- designsystem/bit-cloud/components/header/header.spec.js
+++
@@ -1,3 +0,0 @@
-it('Render', () => {
-  expect(true).toBeTruthy();
-});
