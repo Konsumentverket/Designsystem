10cce8103b0befe512c7a598bfdfb9e1d3cd9e88
Ludwig Lillieborg <ludwig.lillieborg@frojd.se>
2024-09-12 10:44:27 +0200

    Fix accessibility for autocomplete

--- designsystem/bit-cloud/components/input-autocomplete/input-autocomplete.css.js
+++ designsystem/bit-cloud/components/input-autocomplete/input-autocomplete.css.js
@@ -9,6 +9,17 @@ export const containerHasSuggestionsStyle = css`
   box-shadow: 0px 4px 20px 0px rgba(22, 34, 89, 0.10);
 `;
 
+export const labelStyle = css`
+  border: 0;
+  clip: rect(0 0 0 0);
+  height: 1px;
+  margin: -1px;
+  overflow: hidden;
+  padding: 0;
+  position: absolute;
+  width: 1px;
+`;
+
 export const inputStyle = css`
   display: block;
   width: 100%;
@@ -18,10 +29,7 @@ export const inputStyle = css`
   color: ${newColors.shades.kovBlack50};
   padding: 16px;
   border-radius: 8px;
-  border-top: 1px solid ${newColors.shades.fullBlue};
-  border-left: 1px solid ${newColors.shades.fullBlue};
-  border-right: 1px solid ${newColors.shades.fullBlue};
-  border-bottom: 1px solid ${newColors.shades.fullBlue};
+  border: 1px solid ${newColors.shades.fullBlue};
 
   &::placeholder {
     color: ${newColors.secondaries.fullGrey};
@@ -30,9 +38,6 @@ export const inputStyle = css`
 `;
 
 export const inputHasSuggestionsStyle = css`
-  border-top: 1px solid ${newColors.shades.fullBlue};
-  border-left: 1px solid ${newColors.shades.fullBlue};
-  border-right: 1px solid ${newColors.shades.fullBlue};
   border-bottom: none;
   border-radius: 8px 8px 0px 0px;
 `;
@@ -44,22 +49,42 @@ export const dropdownWrapperStyle = css`
   background-color: ${newColors.kovWhite};
   color: ${newColors.shades.kovBlack50};
   border: 1px solid ${newColors.shades.fullBlue};
-  padding: 0 24px;
+  padding: 0;
   border-radius: 0px 0px 8px 8px;
+  overflow: hidden;
 `;
 
 export const dropdownItemStyle = css`
-  padding: 12px 0;
   margin: 0;
-  border-bottom: 1px solid ${newColors.primaries.lightBlue};
+  padding: 0;
+
+  &[aria-selected=true] {
+    border-bottom: 1px solid transparent;
+    background-color: ${newColors.primaries.lightBlue};
+  }
 `;
 
 export const dropdownButtonStyle = css`
+  display: block;
+  font-size: 1.6rem;
+  line-height: 1.5;
+  width: 100%;
+  padding: 12px 24px;
+  text-align: left;
   color: ${newColors.shades.fullBlue};
   font-family: inherit;
   margin: 0;
-  padding: 0;
   background-color: transparent;
+  border-bottom: 1px solid ${newColors.primaries.lightBlue};
+
+  &:hover {
+    border-bottom: 1px solid transparent;
+    background-color: ${newColors.primaries.lightBlue};
+  }
+
+  &:active {
+    background-color: ${newColors.shades.mediumBlue50};
+  }
 `;
 
 export const loadingWrapperStyle = css`
--- designsystem/bit-cloud/components/input-autocomplete/input-autocomplete.js
+++ designsystem/bit-cloud/components/input-autocomplete/input-autocomplete.js
@@ -5,10 +5,12 @@ import { Loading } from '@konsumentverket-sverige/designsystem.loading';
 import {
   containerStyle,
   containerHasSuggestionsStyle,
+  labelStyle,
   inputStyle,
   inputHasSuggestionsStyle,
   dropdownWrapperStyle,
   dropdownItemStyle,
+  dropdownItemActiveStyle,
   dropdownButtonStyle,
   loadingWrapperStyle,
 } from './input-autocomplete.css.js';
@@ -22,9 +24,15 @@ export const InputAutocomplete = ({
   const [suggestions, setSuggestions] = useState([]);
   const [loading, setLoading] = useState(false);
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-  const inputRef = useRef(null);
+  const [activeIndex, setActiveIndex] = useState(-1);
+  const [skipSearch, setSkipSearch] = useState(false);
+  const autocompleteRef = useRef(null);
 
   useEffect(() => {
+    if (skipSearch) {
+      setSkipSearch(false);
+      return;
+    }
     const timer = setTimeout(() => {
       if (query) {
         fetchSuggestions(query);
@@ -43,8 +51,7 @@ export const InputAutocomplete = ({
       const data = await response.json();
 
       const result = data.predictions.map((item) => ({
-        ...item,
-        description: item.description.replace(", Sverige", ""),
+        ...item, description: item.description.replace(', Sverige', ''),
       }));
 
       setSuggestions(result);
@@ -60,10 +67,12 @@ export const InputAutocomplete = ({
 
   const handleInputChange = (event) => {
     setQuery(event.target.value);
+    setActiveIndex(-1);
   };
 
   const handleSuggestionClick = (suggestion) => {
-    setQuery(suggestion.text);
+    setQuery(suggestion.description);
+    setSkipSearch(true);
     setSuggestions([]);
     setIsDropdownOpen(false);
   };
@@ -71,24 +80,59 @@ export const InputAutocomplete = ({
   const handleInputKeyDown = (event) => {
     if (event.key === 'Escape') {
       setIsDropdownOpen(false);
+      return;
+    }
+
+    if (event.key === 'ArrowDown') {
+      setActiveIndex((prevIndex) =>
+        prevIndex < suggestions.length - 1 ? prevIndex + 1 : prevIndex
+      );
+    } else if (event.key === 'ArrowUp') {
+      setActiveIndex((prevIndex) => (prevIndex > 0 ? prevIndex - 1 : prevIndex));
+    } else if (event.key === 'Enter' && activeIndex >= 0) {
+      handleSuggestionClick(suggestions[activeIndex]);
     }
   };
 
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (autocompleteRef.current && !autocompleteRef.current.contains(event.target)) {
+        setIsDropdownOpen(false);
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [autocompleteRef]);
+
+  const showingResult = isDropdownOpen && suggestions.length > 0 && !loading;
+
   return (
-    <div css={[containerStyle, (isDropdownOpen ? containerHasSuggestionsStyle : null)]}>
+    <div
+      ref={autocompleteRef}
+      css={[containerStyle, (showingResult ? containerHasSuggestionsStyle : null)]}
+    >
+      <label
+        css={[labelStyle]}
+        htmlFor="autocomplete-input"
+      >
+        {placeholder}
+      </label>
       <input
-        css={[inputStyle, (isDropdownOpen ? inputHasSuggestionsStyle : null)]}
+        placeholder={placeholder}
+        autoComplete="off"
+        css={[inputStyle, (showingResult ? inputHasSuggestionsStyle : null)]}
         type="text"
         id="autocomplete-input"
-        ref={inputRef}
         value={query}
         onChange={handleInputChange}
         onKeyDown={handleInputKeyDown}
-        placeholder={placeholder}
         aria-autocomplete="list"
         aria-controls={"input-autocomplete-suggestions"}
         aria-expanded={isDropdownOpen}
-        aria-activedescendant={isDropdownOpen ? "input-autocomplete-suggestions" : undefined}
+        aria-activedescendant={activeIndex >= 0 ? `autocomplete-option-${activeIndex}` : undefined}
         aria-describedby={loading ? "loading-indicator" : undefined}
       />
       {loading && (
@@ -104,12 +148,12 @@ export const InputAutocomplete = ({
         >
           {suggestions.map((suggestion, index) => (
             <li
-              css={dropdownItemStyle}
+              css={[dropdownItemStyle, index === activeIndex ? dropdownItemActiveStyle : null]}
               key={index}
               role="option"
               id={`autocomplete-option-${index}`}
+              aria-selected={index === activeIndex}
               onClick={() => handleSuggestionClick(suggestion)}
-              tabIndex="-1"
             >
               <button
                 className={"noStyle"}
