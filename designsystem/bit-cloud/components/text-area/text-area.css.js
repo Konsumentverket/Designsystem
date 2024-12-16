b54ce994c332ceadd66c5bbd3f72fac3ca74adbe
Ida Strindberg <ida.strindberg@frojd.se>
2024-12-16 11:53:11 +0100

    Quick update textarea

--- designsystem/bit-cloud/components/text-area/text-area.css.js
+++ designsystem/bit-cloud/components/text-area/text-area.css.js
@@ -1,32 +1,13 @@
 import { css } from "@emotion/css";
-import { colors } from '@konsumentverket-sverige/designsystem.utils';
+import { colors, newColors } from '@konsumentverket-sverige/designsystem.utils';
 
 export const TextAreaStyle = css`
-    padding: 1.2rem 1.6rem;
-    box-sizing: border-box;
-    border: 2px solid #d0d0d0;
-    font-family: Arial;
-    border-radius: 0.8rem;
-    -khtml-border-radius: 0.8rem; /* this is for the fruit phones */
-    font-size: 1.8rem;
-    line-height: 3rem;
-    -webkit-appearance: textfield; /* safari fix */
-    margin-bottom: 2.4rem;
-    max-width: 100%;
-    min-width: 100%;
-
-    &:focus{
-        outline: none;
-        border-color: ${colors.theme1.mid}
-    }
-
-    &::-webkit-search-cancel-button{
-        appearance: none;
-    }
-    &::placeholder, &::-webkit-input-placeholder {
-        font-style:italic;
-        color: ${colors.theme3.mid};
-    }
+    border-radius: 8px;
+    border: 1px solid ${newColors.shades.fullBlue};
+    padding: 0 16px;
+    width: 100%;
+    font-size: 1.6rem;
+    margin-bottom: 24px;
 `
 
 export const TextAreaWrapperStyle = css`
@@ -35,12 +16,11 @@ export const TextAreaWrapperStyle = css`
 `
 
 export const Label = css`
-  color: #4D4D4D;
-  font-size: 1.8rem;
-  line-height: 3.2rem;
-  margin-bottom: .8rem;
-  display: block;
+    display: block;
+    font-weight: 700;
+    margin-bottom: 18px;
 `
+
 export const invalidStyle = css`
     textarea {
         border-color: #F00;
